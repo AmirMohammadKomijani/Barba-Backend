c2fe1e720dd53445f1bfcc1b4939b3cdb2de01ec
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2023-04-16 10:38:35 +0330

    modify customer profile image situation

--- Customer/models.py
+++ Customer/models.py
@@ -8,6 +8,6 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=255,null=True)
     area = models.CharField(max_length=255)
     phone_Number = models.CharField(max_length=11,unique=True)
-    profile_pic = models.ImageField(upload_to='customer/profile',null=True)
+    profile_pic = models.ImageField(upload_to='customer/profile',null=False,default='default_profile.png')
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
