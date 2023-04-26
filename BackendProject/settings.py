b0072cfd224f6c11ee2510c85cca52c6fa2d457d
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2023-04-26 15:24:27 +0330

    switching dbms

--- BackendProject/settings.py
+++ BackendProject/settings.py
@@ -90,22 +90,22 @@ WSGI_APPLICATION = "BackendProject.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'testTahlil',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'amirmysql2023'
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'testTahlil',
+#         'HOST': 'localhost',
+#         'USER': 'root',
+#         'PASSWORD': 'amirmysql2023'
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
