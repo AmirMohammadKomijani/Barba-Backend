af48a1ebcb6e535fcafe302d49e70d2756252a1a
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2023-05-10 17:46:24 +0330

    customer wallet is ready

--- Customer/views.py
+++ Customer/views.py
@@ -12,7 +12,7 @@ from decimal import Decimal
 
 class CustomerProfileView(ModelViewSet):
     queryset = Customer.objects.all()
-    serializer_class = Customers
+    serializer_class = CustomerWalletSerializer
     permission_classes = [IsAuthenticated]
 
     @action(detail=False, methods=['GET', 'PUT'], permission_classes=[IsAuthenticated])
@@ -28,22 +28,16 @@ class CustomerProfileView(ModelViewSet):
             serializer.save()
             return Response(serializer.data)
 
-    @action(detail=False, methods=['GET','PUT'], permission_classes=[IsAuthenticated],url_path="add_credits", url_name="add_credits")
+    @action(detail=False, methods=['GET', 'PUT'], permission_classes=[IsAuthenticated],url_path="add_credits", url_name="add_credits")
     def add_credits(self, request):
-        (customer, created) = Customer.objects.get_or_create(
-            user_id=request.user.id)
-        
-        if request.method == 'GET':
+            customer= Customer.objects.get(user_id=request.user.id)
+            if request.method == 'PUT':
+                added_credit = Decimal(request.data['credit'])
+                if added_credit < 0:
+                    return Response({"error": "Credit cannot be negative"})        
+                # Transaction.objects.create(customer=customer, transaction_type='C', amount=added_credit)
+                customer.credit += added_credit
+                customer.save()
             serializer = CustomerWalletSerializer(customer)
             return Response(serializer.data)
-        elif request.method == 'PUT':
-            credit = Decimal(request.data['credit'])
-            if credit < 0:
-                return Response({"error": "Credit cannot be negative"}) 
-            customer= Customer.objects.get(user_id=request.user.id)            
-            customer.credit += credit
-            serializer = CustomerWalletSerializer(customer,data=request.data)
-            serializer.is_valid(raise_exception=True)
-            customer.save()
-            return Response(serializer.data) 
 
