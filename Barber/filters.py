5252328f1b8bded7898fa0e37b5c89f672349404
AmirMohammadKomijani <78591495+AmirMohammadKomijani@users.noreply.github.com>
2023-05-19 20:01:57 +0330

    refactoring is completed

--- Barber/filters.py
+++ Barber/filters.py
@@ -2,6 +2,8 @@ from django_filters.rest_framework import FilterSet,DateFromToRangeFilter
 from .models import Barber,OrderServices
 
 
+
+
 class BarberRateFilter(FilterSet):
     class Meta:
         model = Barber
@@ -10,9 +12,11 @@ class BarberRateFilter(FilterSet):
             'rate':['gte','lte']
         }
 
+
+
 class BarberPanelFilter(FilterSet):
-    
     date = DateFromToRangeFilter()
+    
     class Meta:
         model = OrderServices
         fields = ['status','date']
