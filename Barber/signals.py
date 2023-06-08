5e6cf3a5acf2a29f583d374a8ef5a33b7199908f
AmirMohammadKomijani <78591495+AmirMohammadKomijani@users.noreply.github.com>
2023-06-08 19:41:35 +0330

    complete signals by finishing premium and description page automatic add barber

--- Barber/signals.py
+++ Barber/signals.py
@@ -1,9 +1,17 @@
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Barber
+from .models import Barber,BarberDescription,BarberPremium
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_barber_for_new_user(sender, **kwargs):
   if kwargs['created'] and kwargs['instance'].role == 'barber':
     Barber.objects.create(user=kwargs['instance'])
+
+
+
+@receiver(post_save, sender=Barber)
+def create_barber_for_new_user(sender, **kwargs):
+  if kwargs['created']:
+    BarberDescription.objects.create(barber=kwargs['instance'])
+    BarberPremium.objects.create(barber=kwargs['instance'])
